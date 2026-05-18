22c0c8949996a734f08d4764dd098312a9c0b126
HeroInDaClub <151561922+HeroInDaClub@users.noreply.github.com>
2026-05-18 11:16:25 +0300

    Добавьте файлы проекта.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculatorAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Add(5, 3));
+        }
+    }
+}
---
+++ CalculatorAppTests/CalculatorAppTests.cs
@@ -0,0 +1,15 @@
+﻿using CalculatorAPP;
+
+namespace CalculatorAppTests
+{
+    public class CalculatorAppTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(5, 3);
+            Assert.Equal(8, result);
+        }
+    }
+}
